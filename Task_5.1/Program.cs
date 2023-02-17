62535817a4ad306b7b15b75471123e3236eadbd5
Ипполитов Алексей <setgecko@gmail.com>
2023-02-17 07:53:03 +0300

    Выполнил заданиче 5.1

--- Task_5.1/Program.cs
+++ Task_5.1/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            
+            (string Name, string[] Dishes) User;
+
+            Console.WriteLine("Введите имя пользователя");
+            User.Name = Console.ReadLine();
+
+            User.Dishes = new string[5];
+            for(int i = 0; i < User.Dishes.Length; i++)
+            {
+                Console.WriteLine("Введите любимо блюдо номер {0}", i+1);
+                User.Dishes[i] = Console.ReadLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
