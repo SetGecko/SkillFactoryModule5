e56fae8275eca165987f7d1a2aca9ae7d4523856
Ипполитов Алексей <setgecko@gmail.com>
2023-02-17 09:43:11 +0300

    Практика по скринкасту 5.1 + залил результат работы метода в переменну + отобразил перечислением жлементов переменной

---
+++ ScreenCast_5.1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ScreenCast_5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var res2 = new int[5];
+            res2 = GetArrayFromConsole();
+            foreach(var el in res2) 
+            {
+                Console.WriteLine(el);
+            }
+
+        }
+
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+    }
+}
