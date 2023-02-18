6c77969a3543e712d7e5e82fc8e32f6bcb94c867
Ипполитов Алексей <setgecko@gmail.com>
2023-02-18 06:51:29 +0300

    Практика по скринкасту 5.2

--- ScreenCast_5.2/Program.cs
+++ ScreenCast_5.2/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            
+            int[,] arr = {{ - 5, 6, 9, 1, 2, -3},{ -8, 8, 1, 1, 2, -3}
+  };
+
+            SortComplexArray(arr);
+        }
+
+        static void SortComplexArray(int[,] arr)
+        {
+            int temp;
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                    for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
+                        if (arr[i, j] > arr[i, k])
+                        {
+                            temp = arr[i, k];
+                            arr[i, k] = arr[i, j];
+                            arr[i, j] = temp;
+                        }
+            }
         }
     }
 }
