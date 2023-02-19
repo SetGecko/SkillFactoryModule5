feeb2d2ee8fc15b7b3785447bf92e54e73c27bae
Ипполитов Алексей <setgecko@gmail.com>
2023-02-19 08:41:25 +0300

    Выполнил задание 5.3.5

---
+++ Task_5.3.5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_5._3._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 2, 3 };
+            Console.WriteLine(arr[0]);
+
+            BigDataOperation(arr);
+
+            Console.WriteLine(arr[0]);
+        }
+        static void BigDataOperation(in int[] arr)
+        // при получении параметра с int[] arr - получаем данные по ссылке, данные изменятся
+        // при получении параметра с in int[] arr - получаем данные по ссылке, данные изменятся
+        {
+            arr[0] = 4;
+        }
+        /*
+         * По значению передаются параметры int, double, boo
+         * По ссылке передаются параметры int[]
+         */
+    }
+}
