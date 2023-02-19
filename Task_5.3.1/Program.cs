bc3f4c10d7a920f305ce3a51a213735f40a7a28f
Ипполитов Алексей <setgecko@gmail.com>
2023-02-19 08:02:49 +0300

    Выполнил задание 5.3.1

---
+++ Task_5.3.1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_5._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 35;
+            Console.WriteLine("Мой возраст = {0}", myAge);
+
+            ChangeAge(myAge);
+
+            Console.WriteLine("Мой возраст после использования метода = {0}", myAge);
+        }
+
+        static void ChangeAge(int age)
+        {
+            Console.WriteLine("Введите ваш возраст");
+            age = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
