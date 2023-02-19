a58b6c7ec0e9152fc8198f56d4aaeef2795e22be
Ипполитов Алексей <setgecko@gmail.com>
2023-02-19 07:55:15 +0300

    Практика по скринкасту 5.3

---
+++ Screencast_5.3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Screencast_5._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var someName = "Алексей";
+            Console.WriteLine(someName);
+
+            GetName(someName);
+
+            Console.WriteLine(someName);
+        }
+
+        static void GetName(string name)
+        {
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+        }
+    }
+}
