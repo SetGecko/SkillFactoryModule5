94a13985860713a317ce41552002295441075814
Ипполитов Алексей <setgecko@gmail.com>
2023-02-19 09:34:25 +0300

    Выполнил задание 5.4.3

---
+++ Task_5.4.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Task_5._4._3
+{
+    internal class Program
+    {
+        int val = 8;
+        static void Main(string[] args)
+        {
+            int val = 10;
+            Doubled(ref val);
+            Console.WriteLine(val);
+            Console.ReadKey();
+        }
+
+        static void Doubled(ref int val)
+        {
+            val = val * 2;
+        }
+    }
+}
