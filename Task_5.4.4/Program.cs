793d06e1d1ac9b282dcbb64850089b5d480d26a8
Ипполитов Алексей <setgecko@gmail.com>
2023-02-19 09:36:19 +0300

    Выполинл задание 5.4.4

---
+++ Task_5.4.4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_5._4._4
+{
+    internal class Program
+    {
+        int val = 8;
+        static void Main(string[] args)
+        {
+            int val = 9;
+            {
+                val++;
+            }
+
+            Console.WriteLine(val);
+            Console.ReadKey();
+        }
+    }
+}
