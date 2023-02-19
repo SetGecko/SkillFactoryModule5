7d08c60f5b68b5518dd8a5c1c9162951705ecf85
Ипполитов Алексей <setgecko@gmail.com>
2023-02-19 08:51:15 +0300

    Выполнил заждание 5.3.8

---
+++ Task_5.3.8/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_5._3._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int[] GetArrayFromConsole(ref int num)
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+
+        static int[] SortArray(int[] result)
+        {
+            int temp = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
+
