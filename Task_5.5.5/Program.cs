ac0ac4e3849cf28a203256337b334b8c40da974c
Ипполитов Алексей <setgecko@gmail.com>
2023-02-21 09:10:08 +0300

    Выполнил задание 5.5.5

---
+++ Task_5.5.5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_5._5._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(20));
+        }
+
+        static int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+    }
+}
