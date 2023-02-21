fc7d5455a9bcf70b13058581072ad4b6c4be8cd5
Ипполитов Алексей <setgecko@gmail.com>
2023-02-21 08:29:14 +0300

    Программа эхо v1

---
+++ Echo_5.5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Echo_5._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-то");
+
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Напишите глубину эха");
+
+            var deep = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < deep; i++)
+            {
+                Echo(str);
+            }
+        }
+
+        static void Echo(string saidword)
+        {
+            Console.WriteLine(saidword);
+        }
+    }
+}
