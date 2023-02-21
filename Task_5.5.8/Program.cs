7d8c99220a935e2cdb17dd363d0c5726cde0ef48
Ипполитов Алексей <setgecko@gmail.com>
2023-02-21 09:32:22 +0300

    Выполнил задание 5.5.8

---
+++ Task_5.5.8/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_5._5._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PowerUp(2, 3));
+        }
+        private static int PowerUp(int N, byte pow)
+        {
+            if(pow == 0) 
+            {
+                return 1;
+            }
+            else 
+            {
+                if(pow == 1)
+                {
+                    return N;
+                }
+                else
+                {
+                    return N * PowerUp(N, --pow);
+                }
+            }
+        }
+    }
+}
