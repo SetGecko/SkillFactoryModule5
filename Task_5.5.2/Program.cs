4db1e96d60e15d05fd215aeaf4e766b7b9299a31
Ипполитов Алексей <setgecko@gmail.com>
2023-02-21 08:38:07 +0300

    Выполнил задание 5.5.2

---
+++ Task_5.5.2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_5._5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-то");
+
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Напишите глубину эха");
+
+            var deep = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < deep; i++)
+            {
+                Echo(str, deep);
+            }
+        }
+
+        static void Echo(string saidword, int deep)
+        {
+            var modif = saidword;
+
+            Console.WriteLine(saidword);
+
+            if (deep > 1)
+            {
+                Echo(saidword, deep - 1);
+            }
+        }
+    }
+}
